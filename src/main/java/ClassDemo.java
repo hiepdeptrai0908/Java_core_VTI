6eaabb2a54807de72dde25656a4fb0570c955e92
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2024-06-01 21:56:08 +0700

    update

--- src/main/java/ClassDemo.java
+++ src/main/java/ClassDemo.java
@@ -1,5 +1,8 @@
 public class ClassDemo {
     public static void main(String[] args) {
         Dog dog = new Dog();
+        dog.name = "shiba";
+        dog.age = 18;
+        System.out.println(dog.name);
     }
 }
